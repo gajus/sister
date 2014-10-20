f9a285ff6a2430bc1248879ec4a826e2733eb048
Gajus Kuizinas <gk@anuary.com>
2014-10-20 14:36:21 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var pkg = require('./package.json'),
+    gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    del = require('del'),
+    exec = require('child_process').exec;
+
+gulp.task('lint', function () {
+    return gulp
+        .src('./src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('clean', ['lint'], function (cb) {
+    del(['dist'], cb);
+});
+
+gulp.task('distribute', ['clean'], function () {
+    return gulp
+        .src('./dist/sister.js')
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/*', './package.json'], ['default']);
+});
+
+gulp.task('travis', ['default'], function (cb) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, cb);
+});
+
+gulp.task('default', ['distribute']);
