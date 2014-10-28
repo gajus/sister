9d0f325215084b34444a83633dc46932f8f869a4
Gajus Kuizinas <gk@anuary.com>
2014-10-28 18:58:44 +0200

    Using jsonfile to manage config. require() can get cached.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-var pkg = require('./package.json'),
-    karma = require('karma').server,
+var karma = require('karma').server,
     gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     header = require('gulp-header'),
@@ -7,7 +6,8 @@ var pkg = require('./package.json'),
     uglify = require('gulp-uglify'),
     browserify = require('gulp-browserify'),
     fs = require('fs'),
-    del = require('del');
+    del = require('del'),
+    jsonfile = require('jsonfile');
 
 gulp.task('lint', function () {
     return gulp
@@ -30,15 +30,17 @@ gulp.task('bundle', ['clean'], function () {
 });
 
 gulp.task('version', ['bundle'], function () {
-    var bower = require('./bower.json');
+    var name = 'sister',
+        pkg = jsonfile.readFileSync('./package.json'),
+        bower = jsonfile.readFileSync('./bower.json');
 
     gulp
-        .src('./dist/sister.js')
-        .pipe(header('/**\n * @version <%= version %>\n * @link https://github.com/gajus/sister for the canonical source repository\n * @license https://github.com/gajus/sister/blob/master/LICENSE BSD 3-Clause\n */\n', {version: pkg.version}))
+        .src('./dist/' + name + '.js')
+        .pipe(header('/**\n * @version <%= version %>\n * @link https://github.com/gajus/' + name + ' for the canonical source repository\n * @license https://github.com/gajus/' + name + '/blob/master/LICENSE BSD 3-Clause\n */\n', {version: pkg.version}))
         .pipe(gulp.dest('./dist/'))
         .pipe(uglify())
-        .pipe(rename('sister.min.js'))
-        .pipe(header('/**\n * @version <%= version %>\n * @link https://github.com/gajus/sister for the canonical source repository\n * @license https://github.com/gajus/sister/blob/master/LICENSE BSD 3-Clause\n */\n', {version: pkg.version}))
+        .pipe(rename(name + '.min.js'))
+        .pipe(header('/**\n * @version <%= version %>\n * @link https://github.com/gajus/' + name + ' for the canonical source repository\n * @license https://github.com/gajus/' + name + '/blob/master/LICENSE BSD 3-Clause\n */\n', {version: pkg.version}))
         .pipe(gulp.dest('./dist/'));
 
     bower.name = pkg.name;
@@ -48,7 +50,7 @@ gulp.task('version', ['bundle'], function () {
     bower.license = pkg.license;
     bower.authors = [pkg.author];
 
-    fs.writeFile('./bower.json', JSON.stringify(bower, null, 4));
+    jsonfile.writeFileSync('./bower.json', bower);
 });
 
 gulp.task('watch', function () {
